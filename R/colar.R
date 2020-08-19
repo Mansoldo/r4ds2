c91b743fb53c5b6de24172f0c8c50ae6fc9e382d
Mansoldo <dmansoldo@gmail.com>
2020-08-19 17:16:47 -0300

    Initial commit

---
+++ R/colar.R
@@ -0,0 +1,13 @@
+
+
+#' Concatenate two strings
+#' @param nome First string
+#' @param sobrenome Second string
+#' @export
+#'
+colar <-  function (nome, sobrenome) {
+  paste(nome, sobrenome)
+}
+
+
+
